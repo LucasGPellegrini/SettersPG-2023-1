cf7a849457a6cc38678d25507e2615a4885085ff
Pedro Brugger <pedromurilobrugger@hotmail.com>
2024-03-11 21:18:37 -0300

    add new modifies in j

--- solucoes/j.cpp
+++ solucoes/j.cpp
@@ -1,112 +1,51 @@
 #include <iostream>
-#include <vector>
 #include <string>
+#include <vector>
 #include <cmath>
-#include <chrono>
+#include "exprtk.hpp"
+
 using namespace std;
-using namespace std::chrono;
 
-bool evaluateExpression(const string& expr, int k) {
-    int result = 0;
-    string currentNum = "";
-    char lastOperator = '+';
+typedef exprtk::expression<double> expression_t;
+typedef exprtk::parser<double> parser_t;
 
-    for (char c : expr) {
-        if (isdigit(c)) {
-            currentNum += c;
-        } else {
-            int num = stoi(currentNum);
-            switch (lastOperator) {
-                case '+':
-                    result += num;
-                    break;
-                case '-':
-                    result -= num;
-                    break;
-                case '*':
-                    result *= num;
-                    break;
-                case '/':
-                    result /= num;
-                    break;
-                case '^':
-                    result = pow(result, num);
-                    break;
-                default:
-                    break;
-            }
-            currentNum = "";
-            lastOperator = c;
-        }
-    }
-
-    int num = stoi(currentNum);
-    switch (lastOperator) {
-        case '+':
-            result += num;
-            break;
-        case '-':
-            result -= num;
-            break;
-        case '*':
-            result *= num;
-            break;
-        case '/':
-            result /= num;
-            break;
-        case '^':
-            result = pow(result, num);
-            break;
-        default:
-            break;
-    }
-
-    return result == k;
+bool evaluateExpression(const string& expression, double k) {
+    parser_t parser;
+    expression_t expr;
+    expr.register_symbol_table("k", k);
+    parser.compile(expression, expr);
+    return expr.value();
 }
 
-bool solve(const vector<string>& expressions, int k, int currentIndex) {
-    if (currentIndex == expressions.size()) {
-        return true;
-    }
-
-    for (int i = 0; i <= k; ++i) {
-        if (evaluateExpression(expressions[currentIndex], i)) {
-            if (solve(expressions, k, currentIndex + 1)) {
-                return true;
-            }
+bool checkExpressions(const vector<string>& expressions, double k) {
+    for (const string& expr : expressions) {
+        if (!evaluateExpression(expr, k)) {
+            return false;
         }
     }
-
-    return false;
+    return true;
 }
 
 int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+
     int n;
     cin >> n;
-
     vector<string> expressions(n);
+
     for (int i = 0; i < n; ++i) {
-        cin >> expressions[i];
+        cin.ignore();
+        getline(cin, expressions[i]);
     }
 
-    int k = 0;
-
-    auto start = high_resolution_clock::now();
-
-    while (true) {
-        if (solve(expressions, k, 0)) {
-            cout << k << endl;
-            break;
-        }
-        ++k;
-
-        auto end = high_resolution_clock::now();
-        auto duration = duration_cast<seconds>(end - start).count();
-        if (duration > 1) {
-            cout << "Time limit exceeded!" << endl;
-            break;
+    for (int k = 0; k <= 100; ++k) {
+        if (checkExpressions(expressions, k)) {
+            cout << k << "\n";
+            return 0;
         }
     }
 
+    cout << "0\n";
     return 0;
 }
