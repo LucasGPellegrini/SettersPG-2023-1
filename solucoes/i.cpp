c56571b5a442e5007648eb75ac1d520599829a46
Pedro Brugger <pedromurilobrugger@hotmail.com>
2024-03-12 20:02:40 -0300

    fix i and j

--- solucoes/i.cpp
+++ solucoes/i.cpp
@@ -1,37 +1,30 @@
 #include <iostream>
 #include <vector>
-#include <unordered_map>
-#include <algorithm>
-#include <ctime>
 
 using namespace std;
 
-int main() {
-    vector<int> primes = {2, 3, 5, 7, 11, 13, 17, 19, 23, 29};
-
-    string code;
-    cin >> code;
-
-    time_t start = time(nullptr);
-
-    unordered_map<char, int> digit_counts;
-    string decrypted_code = "";
-
-    for (char digit : code) {
-        digit_counts[digit]++;
-        int decrypted_digit = (digit - '0' + digit_counts[digit] - 1) % primes[decrypted_code.size()];
-        decrypted_code += to_string(decrypted_digit);
+bool isPrime(int n) {
+    if (n <= 1)
+        return false;
+    for (int i = 2; i * i <= n; i++) {
+        if (n % i == 0)
+            return false;
     }
-
-    time_t end = time(nullptr);
-    double response_time = difftime(end, start);
-
-    if (response_time <= 1.0) {
-        cout << decrypted_code << endl;
-    } else {
-        cout << "Response time exceeded!" << endl;
-    }
-
-    return 0;
+    return true;
 }
 
+int countPrimes(int N) {
+    int count = 0;
+    for (int i = 1; i <= N; i++) {
+        if (isPrime(i))
+            count++;
+    }
+    return count;
+}
+
+int main() {
+    int N;
+    cin >> N;
+    cout << countPrimes(N) << endl;
+    return 0;
+}
