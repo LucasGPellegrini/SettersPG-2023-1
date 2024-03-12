87d8db4e21058dbbe3888d8b967332af2a71736f
DaviVilarinho <davi.r.vilarinho@gmail.com>
2024-03-12 20:40:17 -0300

    fix marcelo c

--- solucoes/c.cpp
+++ solucoes/c.cpp
@@ -1,38 +1,40 @@
 
+#include <algorithm>
 #include <iostream>
 #include <string>
 #include <vector>
-#include <algorithm>
-
 
 typedef std::pair<std::string, int> NomeNotaPair;
+typedef std::pair<int, NomeNotaPair> IndiceNomeNotaPairPair;
 
-
-bool sortByNotaDecrescente(const NomeNotaPair& a, const NomeNotaPair& b) {
-    return a.second > b.second;
+bool sortByNotaDecrescente(const IndiceNomeNotaPairPair& a,
+                           const IndiceNomeNotaPairPair& b) {
+  if (a.second.second == b.second.second) {
+    return a.first < b.first;
+  }
+  return a.second.second > b.second.second;
 }
 
 int main() {
-    int numNomes;
-    std::cin >> numNomes;
+  int numNomes;
+  std::cin >> numNomes;
 
+  std::vector<IndiceNomeNotaPairPair> nomesNotas;
 
-    std::vector<NomeNotaPair> nomesNotas;
+  for (int i = 0; i < numNomes; ++i) {
+    std::string nome;
+    int nota;
+    std::cin >> nome >> nota;
+    nomesNotas.push_back(std::make_pair(i, std::make_pair(nome, nota)));
+  }
 
+  std::sort(nomesNotas.begin(), nomesNotas.end(), sortByNotaDecrescente);
 
-    for (int i = 0; i < numNomes; ++i) {
-        std::string nome;
-        int nota;
-        std::cin >> nome >> nota;
-        nomesNotas.push_back(std::make_pair(nome, nota));
-    }
+  for (std::vector<IndiceNomeNotaPairPair>::const_iterator it =
+           nomesNotas.begin();
+       it != nomesNotas.end(); ++it) {
+    std::cout << it->second.first << " " << it->second.second << std::endl;
+  }
 
-    std::sort(nomesNotas.begin(), nomesNotas.end(), sortByNotaDecrescente);
-
-
-    for (std::vector<NomeNotaPair>::const_iterator it = nomesNotas.begin(); it != nomesNotas.end(); ++it) {
-        std::cout << it->first << " " << it->second << std::endl;
-    }
-
-    return 0;
+  return 0;
 }
